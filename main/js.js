cc2d4c7227a2742caaed5dbba1d6753b6c3d518a
Maria <zweitesherz@mail.ru>
2020-06-23 22:33:19 +0500

    comments, quest in vue

--- main/js.js
+++ main/js.js
@@ -1,45 +1,62 @@
+Vue.component('question', {
+    props: ['text'],
+    template: '<div class="firepool_body-quest"><p class="question">{{ text }}</p></div>'
+});
 
-    new Vue({
-        el: '#firepool',
-        data: {
-            comment:'',
-            picked:'',
-            question:   document.querySelector('.question').innerText,
+Vue.component('response', {
+    props: ['value','id','type'],
+    template: '<div class="form_radio"> <input  :id="id" :type="type" name="radio" :value="value" onchange="document.getElementById(\'submit\').disabled = !this.checked;"><label :for="id">{{ value }}</label></div>'
+});
 
+Vue.component('response-comment', {
+    props: ['value','id','type'],
+    template: '<div class="form_radio"> <input  :id="id" :type="type" name="radio" :value="value" onchange="document.getElementById(\'submit\').disabled = !this.checked;"><label :for="id">{{ value }}</label> <label  :for="id" class="comment"><p>Введите комментарий</p> <textarea class="form-control"></textarea></label></div>'
+
+
+
+});
+
+new Vue({
+    el: '#firepool',
+    data: {
+        comment:'',
+        picked:'',
+        //question:   document.querySelector('.question').innerText,
+
+    },
+
+    methods: {
+        changeClass() {
+            if(document.getElementById('firepool').classList.contains('firepool-down') === true) {
+                document.getElementById('firepool').classList.remove('firepool-down');
+                document.getElementById('firepool-icon').src = "svg/krest.svg"
+            }
+            else {
+                document.getElementById('firepool').classList.add('firepool-down');
+                document.getElementById('firepool-icon').src = "svg/razvernut.svg"
+            }
+        },
+        btnDown () {
+
+
+            document.getElementById('firepool').classList.add('firepool-down-final');
+            document.getElementById('firepool').classList.remove('firepool-down');
         },
 
-        methods: {
-            changeClass() {
-                if(document.getElementById('firepool').classList.contains('firepool-down') === true) {
-                    document.getElementById('firepool').classList.remove('firepool-down');
-                    document.getElementById('firepool-icon').src = "svg/krest.svg"
-                }
-                else {
-                    document.getElementById('firepool').classList.add('firepool-down');
-                    document.getElementById('firepool-icon').src = "svg/razvernut.svg"
-                }
-            },
-            btnDown () {
 
+        onSubmit: function () {
+            axios.post('https://reqres.in/api/register', {
+                question: this.question,
+                resp: this.picked
 
-                document.getElementById('firepool').classList.add('firepool-down-final');
-                document.getElementById('firepool').classList.remove('firepool-down');
-            },
-
-
-            onSubmit: function () {
-                axios.post('https://reqres.in/api/register', {
-                    question: this.question,
-                    resp: this.picked
-
-                })
-                .then(function (response) {
-                    console.log(response);
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-            }
+            })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
         }
-    });
+    }
+});
 
